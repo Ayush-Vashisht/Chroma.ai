19480adc9ec1ddce13cd67af28cb6da9d9e47cc1
Ayush-Vashisht <125264127+Ayush-Vashisht@users.noreply.github.com>
2024-08-05 23:26:33 +0530

    Add test email and psswd

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="auth">{children}</main>
+    <main className="auth flex flex-col items-center justify-center min-h-screen">
+      {children}
+      <div className="mt-4 text-center">
+        <div>Test Email: chroma+clerk_test@gmail.com</div>
+        <div>Test password: chromaaitest</div>
+      </div>
+    </main>
   )
 }
 
